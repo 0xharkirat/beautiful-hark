f52cb422c962a912c728c30bac5f83645de9bf32
0xharkirat <sandhukirat23@gmail.com>
2026-04-02 13:04:57 +1100

    Fix section prop typing for build

--- components/layout/section.tsx
+++ components/layout/section.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 import { cn } from '../../lib/utils';
 
-interface SectionProps extends React.HTMLProps<HTMLElement> {
+interface SectionProps extends React.ComponentPropsWithoutRef<'section'> {
   background?: string;
   children: ReactNode;
   size?: 'default' | 'wide' | 'full';
